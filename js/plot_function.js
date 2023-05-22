dcce0bf7eb48a1521694b6e6cc23f131b10ee85a
gol-o <134075514+gol-o@users.noreply.github.com>
2023-05-22 03:24:56 +0200

    .

---
+++ js/plot_function.js
@@ -0,0 +1,26 @@
+var rgb = (r, g, b) => {
+    return (r & 0xF0 ? '#' : '#0') + (r << 16 | g << 8 | b).toString(16)
+}
+
+var angle = 0;
+
+setInterval(() => {
+    document.body.style.color = rgb(angle * 10 % 255, 0, 0);
+
+    if (angle < 20)
+        document.write("■".repeat(Math.abs(100 * Math.sqrt(angle * 6 * Math.PI / 180))) + "<p>");
+    else if (angle < 50)
+        document.write("■".repeat(Math.abs(100 * Math.sin(angle * 6 * Math.PI / 180))) + "<p>");
+    else if (angle < 80)
+        document.write("■".repeat(Math.abs(100 * Math.cos(angle * 6 * Math.PI / 180))) + "<p>");
+    else if (angle < 120)
+        document.write("■".repeat(Math.abs(100 * Math.tan(angle * 6 * Math.PI / 180))) + "<p>");
+    else
+        document.write("■".repeat(Math.abs(100 * 1 / Math.tan(angle * 6 * Math.PI / 180))) + "<p>");
+    
+    window.scrollTo(0, angle * 100);
+
+    angle++;
+    angle = angle % 360;
+}, 50);
+
