30da839b3c73e9e5d0a0b7b9eeb0f3698accceec
gol-o <134075514+gol-o@users.noreply.github.com>
2023-05-24 22:22:22 +0200

    Update plot_function.js

--- js/plot_function.js
+++ js/plot_function.js
@@ -8,15 +8,15 @@ setInterval(() => {
     document.body.style.color = rgb(angle * 10 % 255, 0, 0);
 
     if (angle < 20)
-        document.write("■".repeat(Math.abs(100 * Math.sqrt(angle * 6 * Math.PI / 180))) + "<p>");
+        document.write("■".repeat(Math.abs(50 * Math.sqrt(angle * 6 * Math.PI / 180))) + "<p>");
     else if (angle < 50)
-        document.write("■".repeat(Math.abs(100 * Math.sin(angle * 6 * Math.PI / 180))) + "<p>");
+        document.write("■".repeat(Math.abs(50 * Math.sin(angle * 6 * Math.PI / 180))) + "<p>");
     else if (angle < 80)
-        document.write("■".repeat(Math.abs(100 * Math.cos(angle * 6 * Math.PI / 180))) + "<p>");
+        document.write("■".repeat(Math.abs(50 * Math.cos(angle * 6 * Math.PI / 180))) + "<p>");
     else if (angle < 120)
-        document.write("■".repeat(Math.abs(100 * Math.tan(angle * 6 * Math.PI / 180))) + "<p>");
+        document.write("■".repeat(Math.abs(50 * Math.tan(angle * 6 * Math.PI / 180))) + "<p>");
     else
-        document.write("■".repeat(Math.abs(100 * 1 / Math.tan(angle * 6 * Math.PI / 180))) + "<p>");
+        document.write("■".repeat(Math.abs(50 * 1 / Math.tan(angle * 6 * Math.PI / 180))) + "<p>");
     
     window.scrollTo(0, angle * 100);
 
