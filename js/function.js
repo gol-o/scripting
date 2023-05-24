fc3ee7c524c998430b33ba58d340fd549d7f8341
gol-o <134075514+gol-o@users.noreply.github.com>
2023-05-24 22:18:45 +0200

    Update function.js

--- js/function.js
+++ js/function.js
@@ -1,8 +1,15 @@
-var f = function() { alert("first") }
-f()
+// calling anonymous function by function reference
+var f = function() { console.log("f") }
+f() // → f
 
-var g = function() { alert("second") }()
-g() // no calling
+// "iife" function (immediately invoked function expression)
+// g assigned to the return value of the function
+// alert() doesn't return a value
+var g = function() { console.log("g") }()
+g() // → error, g is not a function
+console.log(g) // → undefined
 
-var h = function() { alert("third") }
-()
+// omitting ; sybmol may cause some irregular evaluation order
+var h = function() { console.log("h") }
+(function() { console.log("iife") })()
+// iife function is interpreted as an (unused) argument of the previous function
